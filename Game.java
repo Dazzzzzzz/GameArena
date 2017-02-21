bd6c127ddddb604f9b168b6495b46d792a9e609b
Shaun Fitzsimons <shaunfitz12@gmail.com>
2017-02-21 11:07:41 +0000

    Created main game class with new window functionality

---
+++ Game.java
@@ -0,0 +1,14 @@
+/*
+    *   public class Game
+    *
+    *   Main driver class for the game that contains 
+    *   the main game window
+*/
+
+public class Game
+{
+    public static void main(String args[])
+    {
+        GameArena mainWindow = new GameArena(500, 500);
+    }
+}
