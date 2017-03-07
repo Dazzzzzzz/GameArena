686f6b3cc2ec9c3e64add00b2e3527ede933628e
keshkd <k.krishna-das@lancaster.ac.uk>
2017-03-07 11:49:30 +0000

    I have fixed a bug

--- Game.java
+++ Game.java
@@ -10,5 +10,14 @@ public class Game
     public static void main(String args[])
     {
         GameArena mainWindow = new GameArena(500, 500);
-    }
+		Ball b = new Ball(0, 250, 20, "White");
+		mainWindow.addBall(b);
+		int x = 0; 
+		
+		while (true) {
+			mainWindow.pause();
+			x++;
+			b.setXPosition(x);
+		}
+	}
 }
